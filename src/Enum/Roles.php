5555161974b7e0b8f70c3ad9c9a5e383100abb58
Emmanuel Okeke <emmanix2002@gmail.com>
2018-07-11 01:50:45 +0100

    added enum for roles

---
+++ src/Enum/Roles.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Visacheck\Visacheck\Enum;
+
+
+class Roles
+{
+    const ADMINISTRATOR = 'administrator';
+    const AUTOCENTRE_ADMINISTRATOR = 'autocentre administrator';
+    const COMPANY_ADMINISTRATOR = 'company administrator';
+    const FLEET_MANAGER = 'fleet manager';
+    const GOD = 'god';
+    const INSURANCE_AGENT = 'insurance agent';
+    const MECHANIC = 'mechanic';
+    const MEMBER = 'member';
+}
