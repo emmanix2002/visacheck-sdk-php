a7c7f2525f1db21ddceece4935f694bc29aaceb3
Emmanuel Okeke <emmanix2002@gmail.com>
2018-07-12 21:40:16 +0100

    bug fix for missing request parameters

--- src/Services/Identity/ForgotPassword.php
+++ src/Services/Identity/ForgotPassword.php
@@ -7,6 +7,15 @@ use Visacheck\Visacheck\Services\AbstractService;
 
 class ForgotPassword extends AbstractService
 {
+    /**
+     * @return $this
+     */
+    protected function prefillBody()
+    {
+        $this->body['client_id'] = $this->sdk->getClientId();
+        $this->body['client_secret'] = $this->sdk->getClientSecret();
+        return $this;
+    }
     
     /**
      * Returns the name of the resource.
